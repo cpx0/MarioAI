4070a104910e9e4365e54223c9128a805dac419e
vision <vision2cpx@gmail.com>
2022-06-20 04:59:10 +0900

    refactor demo.py

--- demo.py
+++ demo.py
@@ -12,8 +12,9 @@ from train import parse_args
 def main():
     args = parse_args()
     cfg = cfg_factory[args.agent]
+    # TODO: User should be refactored instead of hard coded following dir and name
     chkpt_dir = Path("checkpoints") / "2022-06-18T16-35-14"
-    chkpt_name = "mario_net_14.chkpt"
+    chkpt_name = args.finetune_from if args.finetune_from else "mario_net_9.chkpt"
     img, img_color = display_color_image(cfg, chkpt_dir, chkpt_name)
     display_grayscale_movie(img, cfg, chkpt_dir, chkpt_name)
     display_color_movie(img_color, cfg, chkpt_dir, chkpt_name)
