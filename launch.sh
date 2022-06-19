d559c8f8699f13aa90f1663352216f1b847731ea
vision <vision2cpx@gmail.com>
2022-06-19 12:18:08 +0900

    fix docker environment

--- launch.sh
+++ launch.sh
@@ -3,14 +3,18 @@
 NB_USER=cpx0
 CMDNAME=`basename $0`
 
-echo "Usage: $CMDNAME container_name image_tag" 1>&2
+echo "Usage: $CMDNAME container_name image_tag"
 
-echo "JupyterLab open at http://127.0.0.1:9280 "
-
-container=${1:-"dm_control"}
-tag=${2:-"1.1"}
+container=${1:-"mario_ai"}
+host_port=${2:-"9280"}
+tag=${3:-"1.1"}
 
 echo "docker container name: $container"
+echo "docker host port for JupyterLab: $host_port"
 echo "docker image 'dm_control:$tag'"
 
-docker run --gpus all -ti --rm --name $container -p 9280:2980 -v $(pwd):/home/$NB_USER/workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix:10.0 dm_control:$tag
+echo "JupyterLab open at http://127.0.0.1:$host_port "
+
+docker run --gpus all -ti --rm --name $container -p $host_port:2980 -v $(pwd):/home/$NB_USER/workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix:10.0 dm_control:$tag
+
+# docker-compose up --no-recreate
