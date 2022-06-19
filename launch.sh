e1f2ab4225c32c9ba21ca9deb5bde7d711d905e3
cpx <74306862+cpx0@users.noreply.github.com>
2022-06-19 21:53:57 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-NB_USER=cpx0
+USER_NAME=cpx0
 CMDNAME=`basename $0`
 
 echo "Usage: $CMDNAME container_name image_tag"
@@ -15,6 +15,6 @@ echo "docker image 'dm_control:$tag'"
 
 echo "JupyterLab open at http://127.0.0.1:$host_port "
 
-docker run --gpus all -ti --rm --name $container -p $host_port:2980 -v $(pwd):/home/$NB_USER/workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix:10.0 dm_control:$tag
+docker run --gpus all -ti --rm --name $container -p $host_port:2980 -v $(pwd):/home/$USER_NAME/workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix:10.0 dm_control:$tag
 
 # docker-compose up --no-recreate
