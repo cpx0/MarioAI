dec53c55c79fe5116263a1083cfd4fba07ad85f4
vision <vision2cpx@gmail.com>
2022-06-21 12:46:48 +0900

    fix custom reward func

--- envs.py
+++ envs.py
@@ -48,19 +48,23 @@ def add_reward(action, curr_info, last_x, last_y, last_score, last_coins, last_s
     # give a penalty when an agent acting to go right(1,2,3,4) or left(6,7,8,9) cannot move
     x_stay_penalty = -1 if abs(curr_info["x_pos"] - last_x) == 0 and action > 0 and action < 5 \
                         or abs(curr_info["x_pos"] - last_x) == 0 and action > 5 and action < 10 else 0
-    # y_stay_penalty = -0.5 if abs(curr_info["y_pos"] - last_y) < 2 else 0
-    score_reward = 0.5 if curr_info["score"] > last_score else 0
+    # give a penalty when an agent acting to go up(2,4,5,7,9,11) or down(10) cannot move
+    y_stay_penalty = -0.5 if abs(curr_info["y_pos"] - last_y) == 0 and action == (2 or 4 or 5 or 7 or 9 or 11) \
+                        or abs(curr_info["y_pos"] - last_y) == 0 and action == 10 else 0
+    # score_reward = 0.5 if curr_info["score"] > last_score else 0
+    score_reward = 0
     # When getting coins, Mario get score, too. 
     # So, when getting coins, an adding reward is (score_reward + coins_reward).
-    coins_reward = 0.25 if curr_info["coins"] > last_coins else 0
+    # coins_reward = 0.25 if curr_info["coins"] > last_coins else 0
+    coins_reward = 0
     if last_status == "fireball":
         status_reward = -0.5 if curr_info["status"] == "tall" or curr_info["status"] == "small" else 0
     elif last_status == "tall":
         status_reward = -0.5 if curr_info["status"] == "small" else (0.5 if curr_info["status"] == "fireball" else 0)
     else:
         status_reward = 0.5 if curr_info["status"] == "fireball" or curr_info["status"] == "tall" else 0
-    # return x_stay_penalty + y_stay_penalty + score_reward + coins_reward + status_reward
-    return x_stay_penalty + score_reward + coins_reward + status_reward
+    
+    return x_stay_penalty + y_stay_penalty + score_reward + coins_reward + status_reward
 
 
 class GrayScaleObservation(gym.ObservationWrapper):
