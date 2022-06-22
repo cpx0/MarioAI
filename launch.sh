ecef5355db550d31c2c00a8d0171d8949aba836e
vision <vision2cpx@gmail.com>
2022-06-22 13:08:08 +0900

    refactor and fix hostname in launch.sh

--- launch.sh
+++ launch.sh
@@ -3,18 +3,29 @@
 USER_NAME=cpx0
 CMDNAME=`basename $0`
 
-echo "Usage: $CMDNAME container_name image_tag"
 
-container=${1:-"mario_ai"}
-host_port=${2:-"9280"}
-tag=${3:-"1.2"}
+function main() {
 
-echo "docker container name: $container"
-echo "docker host port for JupyterLab: $host_port"
-echo "docker image 'dm_control:$tag'"
+    echo "Usage: $CMDNAME container_name host_port image_tag"
 
-echo "JupyterLab open at http://127.0.0.1:$host_port "
+    container=${1:-"mario_ai"}
+    host_port=${2:-"9280"}
+    tag=${3:-"1.2"}
 
-docker run --gpus all -ti --rm --name $container -p $host_port:2980 -v $(pwd):/home/$USER_NAME/workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix:10.0 dm_control:$tag
+    echo "docker container name: $container"
+    echo "docker host port for JupyterLab: $host_port"
+    echo "docker image 'dm_control:$tag'"
 
-# docker-compose up --no-recreate
+    echo "JupyterLab open at http://127.0.0.1:$host_port "
+
+    docker run --gpus all -ti --rm --hostname $(hostname) --name $container \
+        -p $host_port:2980 -v $(pwd):/home/$USER_NAME/workspace \
+        -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+        dm_control:$tag
+
+    # docker-compose up --no-recreate
+
+}
+
+main "$@"
+
